277fdec29a80620254a8d2fda220e90c8cd9bad4
Chris Scheid <justmobiledev1@gmail.com>
2021-01-22 05:27:50 -0500

    first draft

---
+++ app/src/main/java/com/mobiledeveloperblog/camerasample1/FirstFragment.kt
@@ -0,0 +1,132 @@
+package com.mobiledeveloperblog.camerasample1
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.checkSelfPermission
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private val CAMERA_PERMISSION_CODE = 1000
+    private val IMAGE_CAPTURE_CODE = 1001
+    private var imageUri: Uri? = null
+    private var imageView: ImageView? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    private fun requestCameraPermission(): Boolean {
+        var permissionGranted = false
+
+        //if system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val cameraPermissionNotGranted = checkSelfPermission(activity as Context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            val extStoragePermissionNotGranted = checkSelfPermission(activity as Context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+
+            if (cameraPermissionNotGranted || extStoragePermissionNotGranted){
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+                // Display permission dialog
+                requestPermissions(permission, CAMERA_PERMISSION_CODE)
+            }
+            else{
+                // Permission already granted
+                permissionGranted = true
+            }
+        }
+        else{
+            // Android version earlier than M -> no need to request permission
+            permissionGranted = true
+        }
+
+        return permissionGranted
+    }
+
+    // Handle Allow or Deny response from the permission dialog
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode === CAMERA_PERMISSION_CODE) {
+            if (grantResults.size === 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                // Permission was granted
+                openCameraInterface()
+            }
+            else{
+                // Permission was denied
+                showAlert("Camera permission was denied. Unable to take a picture.");
+            }
+        }
+    }
+
+    private fun openCameraInterface() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, R.string.take_picture)
+        values.put(MediaStore.Images.Media.DESCRIPTION, R.string.take_picture_description)
+        imageUri = activity?.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        // Create camera intent
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+
+        // Launch intent
+        startActivityForResult(intent, IMAGE_CAPTURE_CODE)
+    }
+
+    private fun showAlert(message: String) {
+        val builder = AlertDialog.Builder(activity as Context)
+        builder.setMessage(message)
+        builder.setPositiveButton(R.string.ok_button_title, null)
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Callback from camera intent
+        if (resultCode == Activity.RESULT_OK){
+            // Set image captured to image view
+            imageView?.setImageURI(imageUri)
+        }
+        else {
+            // Failed to take picture
+            showAlert("Failed to take camera picture")
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imageView = view.findViewById(R.id.imageview_picture)
+
+        view.findViewById<Button>(R.id.button_take_picture).setOnClickListener {
+            // Request permission
+            val permissionGranted = requestCameraPermission()
+            if (permissionGranted) {
+                // Open the camera interface
+                openCameraInterface()
+            }
+        }
+    }
+}
